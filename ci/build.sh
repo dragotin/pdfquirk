8a299e20379ec9a7be4ecafb419ab7969209717d
Klaas Freitag <kraft@freisturz.de>
2021-12-30 12:02:05 +0100

    Workflow: drop conda with Qt based pdf creator

--- ci/build.sh
+++ ci/build.sh
@@ -44,7 +44,6 @@ env DESTDIR=AppDir ninja install
 # get linuxdeploy and its qt plugin
 wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-"$ARCH".AppImage
 wget https://github.com/TheAssassin/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-"$ARCH".AppImage
-wget -c https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh
 chmod +x linuxdeploy*.{AppImage,sh}
 
 # if deskew is installed, we deploy it using linuxdeploy's -e
@@ -64,13 +63,6 @@ popd
 # build AppImage
 export UPD_INFO="gh-releases-zsync|dragotin|pdfquirk|continuous|PDFQuirk\*-$ARCH.AppImage.zsync"
 
-# we use linuxdeploy-plugin-conda to install a Python environment in <AppDir>/usr/conda and install
-# img2pdf from PyPI to it
-# to call it calculate the path to the main binary, then add ../conda/bin/python -m img2pdf
-export PIP_REQUIREMENTS="img2pdf"
-# Pillow needs to be installed using a conda package because those are relocatable
-export CONDA_PACKAGES="Pillow"
-
-./linuxdeploy-"$ARCH".AppImage --appdir AppDir --plugin conda --plugin qt --output appimage "${extra_ld_args[@]}"
+./linuxdeploy-"$ARCH".AppImage --appdir AppDir --plugin qt --output appimage "${extra_ld_args[@]}"
 
 mv PDFQuirk*.AppImage* "$OLD_CWD"/
